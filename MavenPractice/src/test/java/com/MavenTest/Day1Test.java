583c5390856d98813517053527cd82645cfd731d
gopal <patilsureshc54@gmail.com>
2019-02-04 06:42:33 -0500

    1stPush

---
+++ MavenPractice/src/test/java/com/MavenTest/Day1Test.java
@@ -0,0 +1,12 @@
+package com.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class Day1Test {
+	@Test
+	public void displayMessage()
+	{
+		System.out.println("Print this message");
+	}
+
+}
