0a1d9037ab298c0f086c40a5d0a98d9d6a3660c5
gpatil <gpatil@5119-W7.rvminc.com>
2019-02-04 19:30:13 -0500

    02042019 0729 pm

--- MavenTest/src/test/java/com/SeleniumWork/MavenTest/ReadWriteExcelTest.java
+++ MavenTest/src/test/java/com/SeleniumWork/MavenTest/ReadWriteExcelTest.java
@@ -15,26 +15,23 @@ import org.testng.annotations.Test;
 
 public class ReadWriteExcelTest {
 	
-	@Test
+	
 	public ArrayList<String> getData(String testCaseName) throws IOException
 	{
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\TestData\\TestData.xlsx");
-		
-		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		
-		XSSFSheet sheet = workbook.getSheet("LoginDetails"); //EnterSheet Name
-		
-		Iterator<Row> rows = sheet.iterator();
-		
-		Iterator<Cell> cells;
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\TestData\\TestData.xlsx");		
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);		
+		XSSFSheet sheet = workbook.getSheet("LoginDetails"); //EnterSheet Name		
+		Iterator<Row> rows = sheet.iterator(); //to iterate all the rows		
+		Iterator<Cell> cells;  //to iterate all the cells
 		
 		int totalRows = 0;
 		int totalColumn = 0;
 		int columnNumber1 = 0;
 		int rowNumber1 = 0;
 		
-		ArrayList<String> testData = new ArrayList<String>();
+		ArrayList<String> testData = new ArrayList<String>(); //to save fetched data in ArrayList
 		
+		/*** Get Total Row count and Column count ****/
 		while(rows.hasNext())
 		{
 			Row row = rows.next();
@@ -49,14 +46,16 @@ public class ReadWriteExcelTest {
 				}
 			}
 		}
+		/***************************************/
+		
 		boolean flag = false;
 		break_a:
-		for(int i=0;i<totalRows;i++)
+		for(int i=0;i<totalRows;i++) //traverse all the rows
 		{
 			if(!flag)
 			{
 				break_b:
-				for(int j=0;j<totalColumn;j++)
+				for(int j=0;j<totalColumn;j++) //get the column# having header 'TestCases'
 				{
 					if(sheet.getRow(i).getCell(j).getStringCellValue().equalsIgnoreCase("TestCases"))
 					{
@@ -66,22 +65,21 @@ public class ReadWriteExcelTest {
 					}
 				}
 			}
-			if(flag)
+			if(flag) //once get the column#, get the row# for desired Test Case
 			{
 				if(sheet.getRow(i).getCell(columnNumber1).getStringCellValue().equalsIgnoreCase(testCaseName))
 				{
 					rowNumber1 = i;
 					break break_a;
-				}
-				
+				}				
 			}
 		}
 		
+		/**** now u have column# & row#. Get all the test data from the row **/
 		for(int i=columnNumber1+1;i<totalColumn;i++)
 		{
 			testData.add(sheet.getRow(rowNumber1).getCell(i).getStringCellValue());
 		}
 		return testData;
 	}
-
 }
