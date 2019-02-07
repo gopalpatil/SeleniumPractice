3bea0a9ae46df69ca15727551a6183cf48f4ece9
gpatil <gpatil@5119-W7.rvminc.com>
2019-02-06 20:05:19 -0500

    02062019 2005

--- MavenTest/src/test/java/com/SeleniumWork/MavenTest/Log4jTest_1.java
+++ MavenTest/src/test/java/com/SeleniumWork/MavenTest/Log4jTest_1.java
@@ -1,13 +1,20 @@
 package com.SeleniumWork.MavenTest;
 
-import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.*;
 
 public class Log4jTest_1 {
 	
-	@Test
-	public void loggingInfo()
+	private static Logger log = LogManager.getLogger(Log4jTest_1.class.getName());
+	public static void main(String[] args)
 	{
-		
+
+		log.debug("This is DEBUG");
+		log.info("This is INFO");
+		log.error("This is ERROR");
+		log.fatal("This is FATAL");
+			
+
 	}
 
 }
--- MavenTest/src/test/java/com/SeleniumWork/MavenTest/Log4jTest_3.java
+++ MavenTest/src/test/java/com/SeleniumWork/MavenTest/Log4jTest_3.java
@@ -2,21 +2,17 @@ package com.SeleniumWork.MavenTest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.Test;
 
-public class Log4jTest_2 {
-	
+public class Log4jTest_3 {
 
-	private static Logger log = LogManager.getLogger(Log4jTest_2.class.getName());
-	@Test
-	public void loggingInfo2()
-	{
+	private static Logger log = LogManager.getLogger(Log4jTest_3.class.getName());
+	public static void main(String[] args) {
+
 		log.debug("This is DEBUG");
 		log.info("This is INFO");
 		log.error("This is ERROR");
 		log.fatal("This is FATAL");
-			
-	}
 
+	}
 
 }
