a0de4d8c432000f5ba64384eca7df2e086d4d28e
gpatil <gpatil@5119-W7.rvminc.com>
2019-02-06 19:32:33 -0500

    02062019 1932

---
+++ MavenTest/src/test/java/com/SeleniumWork/MavenTest/Log4jTest_1.java
@@ -0,0 +1,13 @@
+package com.SeleniumWork.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class Log4jTest_1 {
+	
+	@Test
+	public void loggingInfo()
+	{
+		
+	}
+
+}
---
+++ MavenTest/src/test/java/com/SeleniumWork/MavenTest/Log4jTest_2.java
@@ -0,0 +1,13 @@
+package com.SeleniumWork.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class Log4jTest_2 {
+	
+	@Test
+	public void loggingInfo()
+	{
+		
+	}
+
+}
